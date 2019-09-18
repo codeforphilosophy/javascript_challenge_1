c9aa7899d2406b63a0827a0837cbc882da1367f6
codeforphilosophy <sariyya.abdullayeva@gmail.com>
2019-09-18 14:22:39 +0300

    Added: challenge 15

---
+++ JS_challenge_15/JS_15.js
@@ -0,0 +1,17 @@
+// Bir string verilir. Verilen stringi stringin uzunlugu defe coxaldib yazan proqram
+// Tip: stringler de ededler kimi toplana bilir 
+// “men” + “yavru” + “kurtam” = “menyavrukurtam”
+
+// Example: 
+// Verilen_String: “salam” 
+// Output: salamsalamsalamsalamsalam
+
+var str = prompt ("What you would like to repeat? ");
+
+var result= str;
+
+for (var i = 1; i < str.length; i++){
+    result = result + str; 
+}
+
+alert (result);
