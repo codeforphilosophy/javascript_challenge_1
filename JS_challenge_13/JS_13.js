be97cf9975e43b3ef3494e44daf03907232104da
codeforphilosophy <sariyya.abdullayeva@gmail.com>
2019-09-18 13:58:52 +0300

    Added: Challenge 13 and 14

---
+++ JS_challenge_13/JS_13.js
@@ -0,0 +1,16 @@
+// N ededi ve bir string verilir (string.length > N). String sozunun N’ci herfini N defe ekrana yazan proqram
+// Example: 
+// N =3, string = “Azerbaycan” >>>>>>>>> Output: eee
+// N =5, string = “pufdik” >>>>>>>>> Output: iiiii
+
+var num = prompt ("Your Number: ");
+numInt = Number (num);
+
+var word = prompt ("Your word: ");
+var result = word[numInt];
+
+if (word.length > numInt) {
+for (var i = 1; i <= numInt; i++){
+    alert (result = word[numInt]);
+}
+}
---
+++ JS_challenge_14/JS_14.js
@@ -0,0 +1,15 @@
+// Bir string verilir. Verilen stringi stringin uzunlugu (string.length) defe ekrana yazan program.
+// Example:
+// Verilen_String = “Salam”
+// Output:
+// Salam
+// Salam
+// Salam
+// Salam
+// Salam
+
+var str = ("hello");
+
+for (var i = 1; i <= str.length; i++){
+    alert (str);
+}
